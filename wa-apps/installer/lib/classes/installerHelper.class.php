22d6abcdd4258244b8c886bbab5193bdaae8f9bb
Vadim <vadim.beglov@gmail.com>
2014-03-04 17:35:42 +0400

    Installer fix

--- wa-apps/installer/lib/classes/installerHelper.class.php
+++ wa-apps/installer/lib/classes/installerHelper.class.php
@@ -166,6 +166,7 @@ class installerHelper
 
     public static function isDeveloper()
     {
+        return false;
         $result = false;
         $paths = array();
         $paths[] = dirname(__FILE__).'/.svn';
