00b17119c3f0c844e6e831264f1627285319ff63
Vadim <vadim@ams.bd9.ru>
2014-05-23 16:35:55 +0400

    update

--- wa-plugins/payment/platron/lib/config/plugin.php
+++ wa-plugins/payment/platron/lib/config/plugin.php
@@ -5,7 +5,7 @@ return array(
     'icon'        => 'img/platron_mini.png',
     'logo'        => 'img/platron.png',
 	'vendor'      => '983191',
-    'version'     => '1.0.0',
+    'version'     => '1.0.1',
     'locale'      => array('ru_RU', ),
     'type'        => waPayment::TYPE_ONLINE,
 );
--- wa-plugins/shipping/regionalpickup/lib/config/plugin.php
+++ wa-plugins/shipping/regionalpickup/lib/config/plugin.php
@@ -10,6 +10,6 @@ return array(
     'description' => 'Pickup points by regions (customer selects a pickup point from the list).',
     'icon'        => 'img/delivery16.png', //path to module 16x16 icon
     'logo'        => 'img/delivery.png', //path to module logo (recommended logo size: 60x32)
-    'version'     => '1.3',
+    'version'     => '1.3.2',
     'vendor'      => 670917,
 );
