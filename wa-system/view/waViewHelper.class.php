1cb09c00dbaf1f4580cdce426c415d8282bff961
Vadim <vadim.beglov@gmail.com>
2014-12-01 17:22:47 -0500

    Старый стиль

--- wa-system/view/waViewHelper.class.php
+++ wa-system/view/waViewHelper.class.php
@@ -288,7 +288,7 @@ HTML;
     public function css()
     {
         if (wa()->getEnv() == 'backend') {
-            $css = '<link href="'.wa()->getRootUrl().'wa-content/css/wa/wa-1.3.css?v'.$this->version(true).'" rel="stylesheet" type="text/css" >
+            $css = '<link href="'.wa()->getRootUrl().'wa-content/css/wa/wa-1.0.css?v'.$this->version(true).'" rel="stylesheet" type="text/css" >
 <!--[if IE 9]><link type="text/css" href="'.wa()->getRootUrl().'wa-content/css/wa/wa-1.0.ie9.css" rel="stylesheet"><![endif]-->
 <!--[if IE 8]><link type="text/css" href="'.wa()->getRootUrl().'wa-content/css/wa/wa-1.0.ie8.css" rel="stylesheet"><![endif]-->
 <!--[if IE 7]><link type="text/css" href="'.wa()->getRootUrl().'wa-content/css/wa/wa-1.0.ie7.css" rel="stylesheet"><![endif]-->
